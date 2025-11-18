66460d1e41b3936a178e37354c035b030e16539f
sampod76 <sampodnath76@gmail.com>
2025-11-18 11:31:01 +0600

    module diya

---
+++ Pattern.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int i = n - 1; i < n; i++)
+        {
+            printf(" ");
+        }
+    }
+
+    return 0;
+}
---
+++ cpp_module/2_dynamic_mamory/4_Dynamic_array_size_incress.cpp
@@ -0,0 +1,22 @@
+
+// #include <bits/stdc++.h>
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+#include <string>
+#include <cmath>
+using namespace std;
+/*
+input
+
+5 5 4 6 4
+*/
+
+int main()
+{
+
+    return 0;
+}
