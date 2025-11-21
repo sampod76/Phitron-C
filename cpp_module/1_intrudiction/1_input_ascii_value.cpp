5fc95fd10030126b931f4d76c25bc318dc83936e
sampod76 <sampodnath76@gmail.com>
2025-11-21 23:12:23 +0600

    introice

---
+++ cpp_module/1_intrudiction/1_input_ascii_value.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c = 'a';
+    int asciic = c;
+    cout << asciic << endl;
+    // or
+    cout << (int)c << endl; // type custion
+
+    return 0;
+}
