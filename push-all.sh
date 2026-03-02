b629fe43725343c566c988114cad6ea24976764d
Sampod76 <sampodnath76@gmail.com>
2026-03-02 17:00:02 +0600

    input-graps

--- push-all.sh
+++ push-all.sh
@@ -4,6 +4,6 @@ echo "Pushing to main repository..."
 git push || exit 1
 
 echo "Pushing 4_algorithms to algorithms repository..."
-git subtree push --prefix=4_algorithms algorithms main || echo "No new changes in algorithms"
+git subtree push --prefix=4_algorithms algorithms main --force || echo "No new changes in algorithms"
 
 echo "All push complete ✅"
